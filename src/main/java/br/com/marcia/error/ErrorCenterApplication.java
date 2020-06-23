d870205ab90eda6ba091d4f8e804092eeca95656
Marcia Castagna <marciafc.info@gmail.com>
2020-06-23 01:11:40 -0300

    Criando a estrutura inicial

---
+++ src/main/java/br/com/marcia/error/ErrorCenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcia.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorCenterApplication.class, args);
+    }
+
+}
